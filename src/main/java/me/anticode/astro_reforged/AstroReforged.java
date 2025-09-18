60824694aea229ee54a14aa109021ae8f2f2ccf1
Riley Rivera <rileyanticode@gmail.com>
2025-09-17 22:30:51 -0700

    Add Mauveine item

--- src/main/java/me/anticode/astro_reforged/AstroReforged.java
+++ src/main/java/me/anticode/astro_reforged/AstroReforged.java
@@ -1,10 +1,22 @@
 package me.anticode.astro_reforged;
 
+import me.anticode.astro_reforged.init.AstroItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AstroReforged implements ModInitializer {
 
+    public static String modID = "astro_reforged";
+    public static final Logger LOGGER = LogManager.getLogger(modID);
+
     @Override
     public void onInitialize() {
+        AstroItems.initialize();
+    }
+
+    public static Identifier getId (String name) {
+        return new Identifier(modID, name);
     }
 }
---
+++ src/main/java/me/anticode/astro_reforged/init/AstroItems.java
@@ -0,0 +1,24 @@
+package me.anticode.astro_reforged.init;
+
+import me.anticode.astro_reforged.AstroReforged;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class AstroItems {
+    public static final Item MAUVEINE = register(new Item(new Item.Settings()), "mauveine");
+
+    public static Item register (Item item, String id) {
+        Identifier identifier = AstroReforged.getId(id);
+        return Registry.register(Registries.ITEM, identifier, item);
+    }
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> {
+            itemGroup.add(MAUVEINE);
+        });
+    }
+}
